abe42bc92c2028fc96b12c1767a1012ddb89ffdf
GSaikowshik <saikowshikgandikota@gmail.com>
2026-06-05 13:00:33 +0530

    Pattern-14

--- pattern13.py
+++ pattern13.py
@@ -1,10 +1,9 @@
 class Solution:
     def pattern13(self,N):
-        num=1
-        for i in range(1,N+1):
-            for j in range(1,i+1):
-                print(num,end="")
-                num +=1
+        for i in range(N):
+            for j in range(i+1):
+                  alpha = chr(ord("A")+j)
+                  print(alpha,end="")
             print()
 if __name__ == "__main__":
     sol=Solution()
