dbe27137839565ea04749d41cc7a83700015dc99
GSaikowshik <saikowshikgandikota@gmail.com>
2026-06-14 23:12:45 +0530

    Reverse Number

--- BasicMATH/ReverseNum/brute.py
+++ BasicMATH/ReverseNum/brute.py
@@ -1,1 +1,12 @@
-class
+class Solution:
+    def ReverseNUM(self,N):
+        revNUM=0
+        while N > 0:
+            last_digit=N%10
+            revNUM=revNUM*10+last_digit
+            N=N//10
+        return revNUM
+if __name__ == "__main__":
+    sol=Solution()
+    N=int(input())
+    print(sol.ReverseNUM(N))
