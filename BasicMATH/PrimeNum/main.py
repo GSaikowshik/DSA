bc63815f32f50e85732280e8604447727c3b2652
GSaikowshik <saikowshikgandikota@gmail.com>
2026-06-22 22:52:24 +0530

    prime number

---
+++ BasicMATH/PrimeNum/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def primeNum(N):
+        if N%2==0:
+            print("Not a Prime Number")
+        else:
+            print("Prime Number")
+if __name__ == "__main__":
+    N=int(input())
+    Solution.primeNum(N)
