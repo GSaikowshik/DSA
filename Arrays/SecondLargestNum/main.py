b00ce413bd583dfd7bb11a5d9a776238e1843163
GSaikowshik <saikowshikgandikota@gmail.com>
2026-06-24 23:14:53 +0530

    Second Largest Number and Second Small Number in the array

---
+++ Arrays/SecondLargestNum/main.py
@@ -0,0 +1,10 @@
+def ascending_order(arr):
+    arr.sort()
+    return arr[-2]
+def ascending_order2(arr):
+    arr.sort()
+    return arr[1]
+if __name__ == "__main__":
+    arr2=[9,6,3,4,0]
+    print("The largest number in arr1:",ascending_order(arr2))
+    print("The smallest number in arr1:",ascending_order2(arr2))
