183318b6fbc3e0b83ec4089b80aead5956758edc
GSaikowshik <saikowshikgandikota@gmail.com>
2026-06-25 23:25:41 +0530

    ookk

--- Arrays/SecondLargestNum_SecondSmallNum/main.py
+++ Arrays/SecondLargestNum_SecondSmallNum/main.py
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     arr2=[9,6,3,4,0]
     print("The largest number in arr1:",ascending_order(arr2))
     print("The smallest number in arr1:",ascending_order2(arr2))
+    
