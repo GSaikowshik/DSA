977de6f0baf93ef06666462f7f6bbe7d7d691c80
GSaikowshik <saikowshikgandikota@gmail.com>
2026-06-26 23:20:45 +0530

    Armstrong Number

---
+++ BasicMATH/ArmstrongNum/main.py
@@ -0,0 +1,14 @@
+def ArmstrongNum(N):
+    k=len(str(N))
+    num=0
+    while  N>0:
+       ld = N % 10   
+       num+=ld**k
+       N = N // 10  
+    return num == N
+if __name__ == "__main__":
+ N=int(input("Enter Number:")) 
+ if ArmstrongNum(N):
+    print(f"{N} is an Armstrong number.")
+ else:
+    print(f"{N} is not an Armstrong number.")
