f194d44340f0db1b37f88b2e0621ed941866d73f
GANDIKOTA SAIKOWSHIK <saikowshikgandikota@gmail.com>
2026-07-02 21:16:22 +0530

    Fix newline at end of file in main.py

--- BasicMATH/ArmstrongNum/main.py
+++ BasicMATH/ArmstrongNum/main.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
  if ArmstrongNum(N):
     print(f"{N} is an Armstrong number.")
  else:
-    print(f"{N} is not an Armstrong number.") 
+    print(f"{N} is not an Armstrong number.")  
