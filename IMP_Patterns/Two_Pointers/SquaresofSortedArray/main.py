7cbeb302a21b71649559f45049532bc3d964e6d0
GSaikowshik <saikowshikgandikota@gmail.com>
2026-07-17 23:46:32 +0530

    Two Pointer - Squares of a Sorted Array

---
+++ IMP_Patterns/Two_Pointers/SquaresofSortedArray/main.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortedSquares(self, nums: list[int]) -> list[int]:
+        left = 0
+        right = len(nums) - 1
+        result = []
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result.append(nums[left] ** 2)
+                left += 1
+            else:
+                result.append(nums[right] ** 2)
+                right -= 1
+
+        result.reverse()
+
+        return result
+if __name__ == "__main__":
+    sol=Solution()
+    nums1=[-7,-3,2,3,11]
+    print(f"Input: {nums1} \nOutput: {sol.sortedSquares(nums1)}")
+
+    
+    
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
