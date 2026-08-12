9c164ef7d8da5fd335bb149b1dfb4671da4ee02f
GSaikowshik <saikowshikgandikota@gmail.com>
2026-08-13 00:05:24 +0530

    Verifying Anagram Number using Hashmaps

---
+++ Hashmaps/isAnagram/main.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagramm(self,s:str,t:str)->bool:
+        if len(s) != len(t):
+            return False
+        l={}
+        for m in s:
+            if m in l:
+                l[m] += 1
+            else:
+                l[m] = 1
+        for m in t:
+            if m not in l:
+                return False
+            elif l[m] == 1:
+                del l[m]
+            else:
+                l[m] -= 1
+        return True
+if __name__ == "__main__":
+    s = "rat"
+    t = "tar"
+    sol=Solution()
+    k=sol.isAnagramm(s,t)
+    print(k)
