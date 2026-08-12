e68349b744612df8b5dab8137ed9c68aa746bf81
GSaikowshik <saikowshikgandikota@gmail.com>
2026-08-12 20:22:18 +0530

    Contain Duplicate

---
+++ Hashmaps/ContainsDuplicate/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containDuplicate(self,nums:int)->bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            else:
+                s.add(num)
+        return False
+if __name__ == "__main__":
+    nums=[1,2,3,1]
+    sol=Solution()
+    k=sol.containDuplicate(nums)
+    print(k)
