111c01032b782c980d770a9c5a7c660f2328b8f8
GSaikowshik <saikowshikgandikota@gmail.com>
2026-08-18 23:25:55 +0530

    updated in contains duplicate

--- Hashmaps/ContainsDuplicate/main.py
+++ Hashmaps/ContainsDuplicate/main.py
@@ -8,7 +8,7 @@ class Solution:
                 s.add(num)
         return False
 if __name__ == "__main__":
-    nums=[1,2,3,1]
+    nums=[1,2,3,1,2]
     sol=Solution()
     k=sol.containDuplicate(nums)
     print(k)
